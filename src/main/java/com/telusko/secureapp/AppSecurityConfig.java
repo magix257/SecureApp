9a77a91649211511dee01f7229830d4fe85d232f
magix257 <55533437+magix257@users.noreply.github.com>
2019-10-28 10:51:52 +0100

    Spring polaczenie z DB
    nie dziala w pelni prawidlowo

--- src/main/java/com/telusko/secureapp/AppSecurityConfig.java
+++ src/main/java/com/telusko/secureapp/AppSecurityConfig.java
@@ -3,6 +3,7 @@ package com.telusko.secureapp;
 import java.util.List;
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -13,17 +14,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Bean
 	public AuthenticationProvider authProvider()
 	{
 	DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-	
+	provider.setUserDetailsService(userDetailsService);
+	provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
 	return provider;
 	}
 //	@SuppressWarnings("deprecation")
