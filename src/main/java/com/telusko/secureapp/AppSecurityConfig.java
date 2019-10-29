3b7ec7ea293cf17aa202e05a92197adc3afcb5c9
magix257 <55533437+magix257@users.noreply.github.com>
2019-10-29 09:29:56 +0100

    Spring podstawa

--- src/main/java/com/telusko/secureapp/AppSecurityConfig.java
+++ src/main/java/com/telusko/secureapp/AppSecurityConfig.java
@@ -1,22 +1,16 @@
 package com.telusko.secureapp;
 
-import java.util.List;
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
--- src/main/java/com/telusko/secureapp/UserPrincipal.java
+++ src/main/java/com/telusko/secureapp/UserPrincipal.java
@@ -9,6 +9,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 public class UserPrincipal implements UserDetails {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private User user;
 	
 	public UserPrincipal(User user) {
